adad79b8ab6008a07e2e3cbce5df9e1f916de02e
Emerzz35 <160292711+Emerzz35@users.noreply.github.com>
2024-04-18 21:19:48 -0300

    Começo do Show

--- resources/views/produto/show.blade.php
+++ resources/views/produto/show.blade.php
@@ -2,33 +2,50 @@
 <html lang="pt-br">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome</title>
-    <!-- Inclua os arquivos CSS do Bootstrap -->
-    <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
-    <link href="{{ asset('node_modules/bootstrap/dist/bootstrap.min.css') }}" rel="stylesheet">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>{{$produto->PRODUTO_NOME}}</title>
+        <link rel="icon" type="image/x-icon" href="/assets/logo.png">
+        <!--Bootstrap-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+  <link rel="stylesheet" type="text/css" href="/slick/slick.css">
+  <link rel="stylesheet" type="text/css" href="/slick/slick-theme.css">
+  <link rel="stylesheet" href="/css/main-home.css">
 </head>
 
 <body>
-
-<link href="{{ asset('css/styles.css') }}" rel="stylesheet">
-
+<header>
+    <a href="#" class="logo"><img src="/assets/logo.png" alt="Logo Delta"> DELTA </a>
+    <ul class="navlist">
+        <li><a href="home.html" id="pg-atual">Home</a></li>
+        <li><a href="#">Comunidade</a></li>
+        <li><a href="#">Sobre </a></li>
+    </ul>
+    <div class="menu-icons">
+        <box-icon id="search" name='search' size="2rem" color="white"></box-icon>
+        <box-icon id="cart" name='cart' size='2rem' color="white"></box-icon>
+        <box-icon id="user" name='user-circle' size='2rem' color="white"></box-icon>
+    </div>
+    <div class="bx bx-menu" id="menu-icon"></div>
+</header>
 <h1>{{$produto->PRODUTO_NOME}}</h1>
 <main>
-
+   <?php // Carrossel ?>
+   <section class="single-item slider">  
+   @foreach($produto->Imagens as $imagem)
+   @if($produto->Imagens->isNotEmpty())
+   <div class="slide">
+   <img src="{{$imagem->IMAGEM_URL}}" alt="">
+   </div>
+   @endif
+   @endforeach
+   </section>
    <?php // Imagem da direita ?>
 
-   @if($produto->Imagens->isNotEmpty())
-   <img src="{{ $produto->Imagens->first()->IMAGEM_URL }}" alt="">
-   @endif
-
-   <?php // Carrossel ?>
-
-   @foreach($produto->Imagens as $imagem)
-   <img src="{{$imagem->IMAGEM_URL}}" alt="">
-   @endforeach
-
+@if($produto->Imagens->isNotEmpty())
+<img src="{{ $produto->Imagens->first()->IMAGEM_URL }}" alt="">
+@endif
 
    <P>{{$produto->PRODUTO_DESC}}</P>
    <P>Categoria: {{$produto->Categoria->CATEGORIA_NOME}}</P>
@@ -90,7 +107,26 @@
    </div>
 </section>
 
-<script src="{{ asset('node_modules/bootstrap/js/bootstrap.min.js') }}"></script>
+  <!--Script do Slick e do Bootstrap-->
+  <script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+  <script src="/slick/slick.js" type="text/javascript" charset="utf-8"></script>
+  <script type="text/javascript">
+    $(".single-item").slick({
+      variableWidth: true,
+      dots: true,
+      centerMode: true,
+    });
+  </script>
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+      integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+      crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+      integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+      crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+      integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+      crossorigin="anonymous"></script>
+      <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
 </body>
 
 </html>
