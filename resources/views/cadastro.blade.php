41e37d2e63efd384a419967a25bec10d9b76c019
Samantha Souza <samanthaesouza@outlook.com>
2024-05-28 21:26:56 -0300

    CARINHO

--- resources/views/cadastro.blade.php
+++ resources/views/cadastro.blade.php
@@ -70,7 +70,7 @@
         </div>
         <div class="form-group">
             <label for="USUARIO_CPF">CPF</label>
-            <input type="text" class="form-control fr" id="USUARIO_CPF" name="USUARIO_CPF" placeholder="Seu CPF" maxlength="11">
+            <input type="number" class="form-control fr" id="USUARIO_CPF" name="USUARIO_CPF" placeholder="Seu CPF" maxlength="11">
         </div>
         <div class="form-group">
             <label for="USUARIO_SENHA">Senha</label>
